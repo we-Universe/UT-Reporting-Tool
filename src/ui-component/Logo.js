27c835518684189c6cca3580788ff561943a040e
Mayar Abuzahra <79359366+Mayar-Abuzahra@users.noreply.github.com>
2023-12-10 14:32:51 +0200

    first commit

---
+++ src/ui-component/Logo.js
@@ -0,0 +1,11 @@
+// material-ui
+import logo from 'assets/images/UniverseLogo.svg';
+// ==============================|| LOGO SVG ||============================== //
+
+const Logo = () => {
+  return (
+      <img src={logo} alt="Universe" width="140" />
+  );
+};
+
+export default Logo;
