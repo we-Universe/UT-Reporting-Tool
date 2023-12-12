309f9bfd6e1a8f999be4dd0b85a7c6686c0702b9
Mayar Abuzahra <79359366+Mayar-Abuzahra@users.noreply.github.com>
2023-12-12 09:06:11 +0200

    dropdownlist component

--- src/ui-component/Logo.js
+++ src/ui-component/Logo.js
@@ -2,10 +2,20 @@
 import logo from 'assets/images/UniverseLogo.svg';
 // ==============================|| LOGO SVG ||============================== //
 
+import DropdownList from 'ui-component/extended/DropdownList';
+
+const selectedTypes = [
+  'Telecom',
+  'Software Solutions',
+  'Technologies',
+  'E-Marketing',
+];
+
 const Logo = () => {
   return (
     <>
       <img src={logo} alt="Universe" width="140" />
+      <DropdownList selectedTypes={selectedTypes} placeholder={'Choose telecom name'}/>
     </>
   );
 };
---
+++ src/ui-component/extended/DropdownList.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { FormControl, Select, MenuItem, Input } from '@mui/material';
+
+const DropdownList = ({ selectedTypes, placeholder }) => {
+  const [personName, setPersonName] = useState([]);
+
+  const handleChange = (event) => {
+    setPersonName(event.target.value);
+  };
+
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: 48 * 4.5 + 8,
+        width: 250,
+      },
+    },
+  };
+
+  return (
+    <FormControl>
+      <Select
+        multiple
+        displayEmpty
+        value={personName}
+        onChange={handleChange}
+        input={<Input />}
+        renderValue={(selected) => {
+          if (selected.length === 0) {
+            return <em>{placeholder}</em>;
+          }
+          return selected.join(', ');
+        }}
+        MenuProps={MenuProps}
+        inputProps={{ 'aria-label': 'Without label' }}
+      >
+        <MenuItem disabled value="">
+          <em>{placeholder}</em>
+        </MenuItem>
+        {selectedTypes.map((name) => (
+          <MenuItem key={name} value={name}>
+            {name}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default DropdownList;
