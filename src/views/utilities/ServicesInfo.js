10b60ca86f5411b084ee6e9211530daddac5d01c
Tamara-Daghlees <tamara.daghlis@we-universe.com>
2023-12-19 14:54:29 +0200

    changes in Service Info

--- src/views/utilities/ServicesInfo.js
+++ src/views/utilities/ServicesInfo.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // material-ui
-import { Table, TableBody, TableCell, TableHead, TableRow, TextField, Autocomplete } from '@mui/material';
-//import styled from '@emotion/styled';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField,
+  Autocomplete,
+  TableContainer,
+  Select,
+  FormControl,
+  MenuItem,
+  Box
+} from '@mui/material';
+import styled from '@emotion/styled';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
@@ -15,27 +28,23 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import EditButton from 'ui-component/EditButton/EditButton';
 import SaveButton from 'ui-component/SaveButton/SaveButton';
-//import SaveButton from 'ui-component/SaveButton/SaveButton';
 
 // ============================|| UTILITIES SERVICES INFO ||============================ //
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9)
+  { index: 0, name: 'Frozen yoghurt', operatorShare: 159, mtitFile: 'File1', status: 'To', launchedDate: '01/01/2022' },
+  { index: 1, name: 'Ice cream sandwich', operatorShare: 237, mtitFile: 'File2', status: 'CC', launchedDate: '02/02/2022' },
+  { index: 2, name: 'Eclair', operatorShare: 262, mtitFile: 'File3', status: 'To', launchedDate: '03/03/2022' },
+  { index: 3, name: 'Cupcake', operatorShare: 305, mtitFile: 'File4', status: 'CC', launchedDate: '04/04/2022' },
+  { index: 4, name: 'Gingerbread', operatorShare: 356, mtitFile: 'File5', status: 'To', launchedDate: '05/05/2022' }
 ];
 
-// const StyledTableContainer = styled(TableContainer)({
-//   border: '1px solid #ccc',
-//   fontWeight: 'bold',
-//   fontSize: '24px'
-// });
+const StyledTableContainer = styled(TableContainer)({
+  border: '1px solid #ccc',
+  fontWeight: 'bold',
+  fontSize: '24px',
+  borderRadius: '8px'
+});
 
 const top100Films = [
   { label: 'The Shawshank Redemption', year: 1994 },
@@ -50,6 +59,12 @@ const top100Films = [
 const UtilitiesServicesInfo = () => {
   //const [value] = React.useState(new Date('2022-04-17'));
   const theme = useTheme();
+  const [editingRowId, setEditingRowId] = useState(-1);
+  //const [editedRows, setEditedRows] = useState([...rows]);
+
+  const handleEditClick = (index) => {
+    setEditingRowId(index);
+  };
 
   return (
     <MainCard title="Services Info">
@@ -60,40 +75,73 @@ const UtilitiesServicesInfo = () => {
         sx={{ width: '30%', marginBottom: '20px' }}
         renderInput={(params) => <TextField {...params} label="Merchant Name" />}
       />
-
-      <Table>
-        <TableHead sx={{ backgroundColor: theme.palette.primary.light }}>
-          <TableRow>
-            <TableCell>Service Name</TableCell>
-            <TableCell> Operator Share</TableCell>
-            <TableCell>MTIT File </TableCell>
-            <TableCell>Status</TableCell>
-            <TableCell> Launched Date</TableCell>
-            <TableCell>Edit/Save</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row, index) => (
-            <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell component="th" scope="row"></TableCell>
-              <TableCell component="th" scope="row"></TableCell>
-              <TableCell component="th" scope="row"></TableCell>
-              <TableCell>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DesktopDatePicker label=" Launched Date" inputFormat="MM/DD/YYYY" renderInput={(params) => <TextField {...params} />} />
-                </LocalizationProvider>
-              </TableCell>
-              <TableCell>
-                <EditButton />
-                <SaveButton />
-              </TableCell>
+      <StyledTableContainer>
+        <Table>
+          <TableHead>
+            <TableRow sx={{ backgroundColor: theme.palette.primary.light }}>
+              <TableCell>Service Name</TableCell>
+              <TableCell> Operator Share</TableCell>
+              <TableCell>MTIT File </TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell> Launched Date</TableCell>
+              <TableCell>Edit/Save</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell component="th" scope="row">
+                  <TextField
+                    variant="outlined"
+                    sx={{ color: '#0B3782', '& fieldset': { border: 'none' } }}
+                    value={row.name}
+                    InputProps={{ readOnly: editingRowId !== index }}
+                  />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  <TextField
+                    variant="outlined"
+                    sx={{ color: '#0B3782', '& fieldset': { border: 'none' } }}
+                    defaultValue={row.operatorShare}
+                    InputProps={{ readOnly: editingRowId !== index }}
+                  />
+                </TableCell>
+                <TableCell component="th" scope="row"></TableCell>
+
+                <TableCell component="th" scope="row">
+                  {editingRowId !== index ? (
+                    <Box sx={{ backgroundColor: '#f8fafc', padding: '15px', borderRadius: '8px' }}>{row.status}</Box>
+                  ) : (
+                    <FormControl sx={{ width: '100%', color: '#0B3782', '& fieldset': { border: 'none' } }}>
+                      <Select disabled={editingRowId !== index} value={row.status}>
+                        <MenuItem value="To">status 1</MenuItem>
+                        <MenuItem value="CC">status 2</MenuItem>
+                      </Select>
+                    </FormControl>
+                  )}
+                </TableCell>
+                <TableCell>
+                  {editingRowId !== index ? (
+                    <Box sx={{ backgroundColor: '#f8fafc', padding: '15px', borderRadius: '8px' }}> {row.launchedDate}</Box>
+                  ) : (
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DesktopDatePicker
+                        label="Launched Date"
+                        inputFormat="MM/DD/YYYY"
+                        renderInput={(params) => <TextField {...params} />}
+                        value={new Date(row.launchedDate)}
+                      />
+                    </LocalizationProvider>
+                  )}
+                </TableCell>
+                <TableCell sx={{ minWidth: '130px' }}>
+                  {editingRowId === index ? <SaveButton /> : <EditButton handleEditClick={() => handleEditClick(index)} />}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </StyledTableContainer>
     </MainCard>
   );
 };
